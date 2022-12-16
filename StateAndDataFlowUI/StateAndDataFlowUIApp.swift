3f836fd8f4e6bd417382c5e8e599f84d81a70ee0
Pavel <lmr_16@mail.ru>
2022-12-16 15:27:11 +0300

    Initial Commit

---
+++ StateAndDataFlowUI/StateAndDataFlowUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowUIApp.swift
+//  StateAndDataFlowUI
+//
+//  Created by user on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
